4e1efd7dbbbcb7c705abb9147253015a1ab64a79
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-06 19:06:29 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const price = document.querySelector('#price');
+const persons = document.querySelector('#person');
+const tip = document.querySelector('#tip');
+const btn = document.querySelector('.card__data__btn');
+const error = document.querySelector('.card__data__error');
+const result = document.querySelector('.card__data__result');
+const resultSpan = document.querySelector('.card__data__result__cost');
+
+
+
+
