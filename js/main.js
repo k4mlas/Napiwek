ff66fb445126bc1fbfc7a7c6f0176b2c7713f719
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-06 19:18:14 +0200

    update

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ const error = document.querySelector('.card__data__error');
 const result = document.querySelector('.card__data__result');
 const resultSpan = document.querySelector('.card__data__result__cost');
 
+const check = () => {
+	if (price.value !== '' && persons.value !== '' && tip.value !== '') {
+		console.log('Pola są uzupełnione');
+	} else {
+		console.log('Wszystkie pola są puste');
+		error.style.display = 'block';
+	}
+};
 
-
-
+btn.addEventListener('click', check);
